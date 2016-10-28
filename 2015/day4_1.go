eeb9b28035ef828bf5dad2fe30915af020d19aa9
Kristofer Lindberg <krl@spotify.com>
2016-10-28 16:19:36 +0200

    slightly shorter but seems slower

--- 2015/day4_1.go
+++ 2015/day4_1.go
@@ -6,26 +6,15 @@ import (
 	"strconv"
 )
 
-func leadingZeros(start []byte) bool {
-	tails := fmt.Sprintf("0x%x", start[2])
-	tail, _ := strconv.ParseInt(tails, 0, 64)
-	if (fmt.Sprintf("%x", start[0:2]) == "0000") && (tail < 10) {
-		return true
-	} else {
-		return false
-	}
-}
-
-func getDigest(secret string, number int) [16]byte {
-	data := []byte(secret + strconv.Itoa(number))
-	return md5.Sum(data)
-}
-
 func main() {
 	secret := "iwrupvqb"
+
 	for i := 0; i < 2000000; i++ {
-		digest := getDigest(secret, i)
-		if leadingZeros(digest[0:3]) {
+		data := []byte(secret + strconv.Itoa(i))
+		digest := md5.Sum(data)
+		hash := fmt.Sprintf("%x", digest)
+
+		if hash[0:5] == "00000" {
 			fmt.Printf("number is %d and gives the digest %x\n", i, digest)
 			break
 		}
--- 2015/day4_2.go
+++ 2015/day4_2.go
@@ -6,24 +6,15 @@ import (
 	"strconv"
 )
 
-func leadingZeros(start []byte) bool {
-	if fmt.Sprintf("%x", start[0:3]) == "000000" {
-		return true
-	} else {
-		return false
-	}
-}
-
-func getDigest(secret string, number int) [16]byte {
-	data := []byte(secret + strconv.Itoa(number))
-	return md5.Sum(data)
-}
-
 func main() {
 	secret := "iwrupvqb"
+
 	for i := 0; i < 10000000; i++ {
-		digest := getDigest(secret, i)
-		if leadingZeros(digest[0:3]) {
+		data := []byte(secret + strconv.Itoa(i))
+		digest := md5.Sum(data)
+		hash := fmt.Sprintf("%x", digest)
+
+		if hash[0:6] == "000000" {
 			fmt.Printf("number is %d and gives the digest %x\n", i, digest)
 			break
 		}
