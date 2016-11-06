e57e9f707dc1d812d03ab1c4838d6bf13d304aba
Kristofer Lindberg <krl@spotify.com>
2016-11-06 22:20:15 +0100

    it's something

--- 2015/day7_1.go
+++ 2015/day7_1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,65 +11,77 @@ import (
 	"strings"
 )
 
-func logicGate(parts []string, wires map[string]int) int {
-	lvalue := wires[parts[0]]
-	switch parts[1] {
-	case "AND":
-		if rvalue, ok := wires[parts[2]]; ok {
-			return (lvalue & rvalue)
-		} else {
-			return -1
-		}
-	case "OR":
-		if rvalue, ok := wires[parts[2]]; ok {
-			return (lvalue | rvalue)
-		} else {
-			return -1
-		}
-	case "LSHIFT":
-		rtmp, _ := strconv.Atoi(parts[2])
-		rvalue := uint8(rtmp)
-		return (lvalue << uint8(rvalue))
-	case "RSHIFT":
-		rtmp, _ := strconv.Atoi(parts[2])
-		rvalue := uint8(rtmp)
-		return (lvalue >> uint8(rvalue))
-	}
+var wires map[string]int
+
+func gate(gateType string, lvalue, rvalue int) int {
 	return 0
 }
 
-func main() {
-	var wires map[string]int
-	wires = make(map[string]int)
-	input, err := os.Open("input/7")
+func getValue(part string) (int, error) {
+	value, err := strconv.Atoi(part)
 	if err != nil {
-		log.Fatal(err)
+		if value, ok := wires[part]; ok {
+			return value, nil
+		}
+		return 0, errors.New("We have no input")
 	}
-	defer input.Close()
+	return value, nil
+}
 
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		instruction := strings.Split(line, "->")
-		signal := strings.Trim(instruction[0], " ")
-		signalParts := strings.Split(signal, " ")
-		receiver := strings.Trim(instruction[1], " ")
-		// Determine which type of signal we have based on instruction length
-		switch sl := len(signalParts); sl {
-		case 1:
-			value, _ := strconv.Atoi(signalParts[0])
-			wires[receiver] = value
-		case 2:
-			if val, ok := wires[signalParts[1]]; ok {
-				// value := wires[signalParts[1]]
-				wires[receiver] = (65535 - val)
-			}
-		case 3:
-			if _, ok := wires[signalParts[0]]; ok {
-				val := logicGate(signalParts, wires)
-				if val != -1 {
-					wires[receiver] = logicGate(signalParts, wires)
+func applyGate(receiver string, parts []string) bool {
+	lvalue, err := getValue(parts[0])
+	if err != nil {
+		return false
+	}
+	rvalue, err := getValue(parts[2])
+	if err != nil {
+		return false
+	}
+	switch parts[1] {
+	case "AND":
+		wires[receiver] = (lvalue & rvalue)
+	case "OR":
+		wires[receiver] = (lvalue | rvalue)
+	case "LSHIFT":
+		wires[receiver] = (lvalue << uint8(rvalue))
+	case "RSHIFT":
+		wires[receiver] = (lvalue >> uint8(rvalue))
+	}
+	return true
+}
+
+func main() {
+	wires = make(map[string]int)
+	// eh, we should do this in another way
+	for i := 0; i < 150; i++ {
+		input, err := os.Open("input/7")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer input.Close()
+
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			line := scanner.Text()
+			instruction := strings.Split(line, "->")
+			signal := strings.Trim(instruction[0], " ")
+			signalParts := strings.Split(signal, " ")
+			receiver := strings.Trim(instruction[1], " ")
+			// Determine which type of signal we have based on instruction length
+			switch sl := len(signalParts); sl {
+			case 1:
+				value, err := getValue(signalParts[0])
+				if err == nil {
+					// fmt.Printf("We are getting past %s\n", line)
+					wires[receiver] = value
 				}
+			case 2:
+				if val, err := getValue(signalParts[1]); err == nil {
+					// fmt.Printf("We are getting past %s\n", line)
+					wires[receiver] = (65535 - val)
+				}
+			case 3:
+				applyGate(receiver, signalParts)
 			}
 		}
 	}
