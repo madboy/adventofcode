b065660a186513da8770efff7f965c4577a59f76
Kristofer Lindberg <krl@spotify.com>
2016-11-06 22:27:16 +0100

    remove unecessary code

--- 2015/day7_1.go
+++ 2015/day7_1.go
@@ -13,10 +13,6 @@ import (
 
 var wires map[string]int
 
-func gate(gateType string, lvalue, rvalue int) int {
-	return 0
-}
-
 func getValue(part string) (int, error) {
 	value, err := strconv.Atoi(part)
 	if err != nil {
@@ -28,26 +24,21 @@ func getValue(part string) (int, error) {
 	return value, nil
 }
 
-func applyGate(receiver string, parts []string) bool {
-	lvalue, err := getValue(parts[0])
-	if err != nil {
-		return false
+func applyGate(receiver string, parts []string) {
+	lvalue, lerr := getValue(parts[0])
+	rvalue, rerr := getValue(parts[2])
+	if (lerr == nil) && (rerr == nil) {
+		switch parts[1] {
+		case "AND":
+			wires[receiver] = (lvalue & rvalue)
+		case "OR":
+			wires[receiver] = (lvalue | rvalue)
+		case "LSHIFT":
+			wires[receiver] = (lvalue << uint8(rvalue))
+		case "RSHIFT":
+			wires[receiver] = (lvalue >> uint8(rvalue))
+		}
 	}
-	rvalue, err := getValue(parts[2])
-	if err != nil {
-		return false
-	}
-	switch parts[1] {
-	case "AND":
-		wires[receiver] = (lvalue & rvalue)
-	case "OR":
-		wires[receiver] = (lvalue | rvalue)
-	case "LSHIFT":
-		wires[receiver] = (lvalue << uint8(rvalue))
-	case "RSHIFT":
-		wires[receiver] = (lvalue >> uint8(rvalue))
-	}
-	return true
 }
 
 func main() {
