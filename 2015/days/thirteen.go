a4ee46ed66b76c7b35e20f998e07d4caa8f3e7a3
Kristofer Lindberg <k.lindberg@gmail.com>
2018-09-13 19:01:30 +0200

    move test data for day 13

--- 2015/days/thirteen.go
+++ 2015/days/thirteen.go
@@ -80,21 +80,6 @@ func getMaxHappiness(people tools.Set, seatingKeys map[string]int) string {
 
 // Run13 is helping us with optimal placing
 func Run13(scanner *bufio.Scanner) string {
-	// input := []string{
-	// 	"David would gain 46 happiness units by sitting next to Alice.",
-	// 	"David would lose 7 happiness units by sitting next to Bob.",
-	// 	"David would gain 41 happiness units by sitting next to Carol.",
-	// 	"Carol would lose 62 happiness units by sitting next to Alice.",
-	// 	"Carol would gain 60 happiness units by sitting next to Bob.",
-	// 	"Carol would gain 55 happiness units by sitting next to David.",
-	// 	"Alice would gain 54 happiness units by sitting next to Bob.",
-	// 	"Alice would lose 79 happiness units by sitting next to Carol.",
-	// 	"Alice would lose 2 happiness units by sitting next to David.",
-	// 	"Bob would gain 83 happiness units by sitting next to Alice.",
-	// 	"Bob would lose 7 happiness units by sitting next to Carol.",
-	// 	"Bob would lose 63 happiness units by sitting next to David.",
-	// }
-
 	var input []string
 	for scanner.Scan() {
 		input = append(input, scanner.Text())
